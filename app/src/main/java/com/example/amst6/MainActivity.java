c242f02c65dc9cd5caa8c893a6cb743118ce8215
Luis Orlando Armijos Cevallos <loarmijo25@gmail.com>
2019-08-06 18:20:39 -0500

    Creacion Ventana Selection

--- app/src/main/java/com/example/amst6/MainActivity.java
+++ app/src/main/java/com/example/amst6/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.amst6;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void inicio(View view) {
+        Intent i = new Intent(this, SelectionActivity.class );
+        startActivity(i);
+    }
+
 }
